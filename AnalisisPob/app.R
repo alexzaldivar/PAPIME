20b2285817adcd8e2ae8466468807022eb9b61a3
alexzaldivar <al3xvet@gmail.com>
2023-03-23 12:10:19 -0600

    Va la app

---
+++ AnalisisPob/app.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
+
