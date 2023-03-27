6d431a59ba00a98d33038b8ce94a94df3c15c407
Jose Pablo Gomez <josepablogomez@Pablos-MacBook-Pro.local>
2023-03-26 18:20:51 -0700

    codigo para subunidad 1 y reorganizacion de shiny app

---
+++ Codigo/AnalisisPob/server.R
@@ -0,0 +1,5 @@
+# Server --------------
+function(input, output){
+  
+} %>% 
+  shinyServer()
---
+++ Codigo/AnalisisPob/ui.R
@@ -0,0 +1,22 @@
+# Header -----------
+header <- dashboardHeader(
+  
+)
+
+# Sidebar -----------
+sidebar <- dashboardSidebar(
+  
+)
+
+# Body --------
+body <- dashboardBody(
+  
+)
+
+
+# Run the UI
+dashboardPage(
+  header = header, 
+  sidebar = sidebar, 
+  body = body
+) %>% shinyUI()
