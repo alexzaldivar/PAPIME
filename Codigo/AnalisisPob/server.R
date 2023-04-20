ca73402868af416623cab1099d1b07f84ff5a242
Jose Pablo Gomez <josepablogomez@Pablos-MacBook-Pro.local>
2023-04-20 12:12:01 -0700

    agregar notas de las comunidades

--- Codigo/AnalisisPob/server.R
+++ Codigo/AnalisisPob/server.R
@@ -23,11 +23,11 @@ function(input, output){
     
     ggplotly(p)
   })
+  
   output$Grid <- renderPlotly({
     p <- mosquitos %>%
       ggplot() +
       geom_tile(aes(x = x, y = y, fill = !!sym(input$var)), col = 'black') +
-      # facet_wrap(~SITIO) + #
       theme_void()
 
     ggplotly(p)
