457a21db972d4ac5580589356b240befb5dae4f1
alexzaldivar <al3xvet@gmail.com>
2023-08-11 13:43:54 -0600

    .

--- Codigo/SampleSize/app.R
+++ Codigo/SampleSize/app.R
@@ -49,7 +49,6 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot"),
            tableOutput("Resultados")
         )
     )
@@ -58,21 +57,8 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-  output$n <-
-    
 
   
-  
-  output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
 }
 
 # Run the application 
